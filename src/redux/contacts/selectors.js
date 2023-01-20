750e5a06bab08341f65f4e29b042897a72d57022
Xdecent <xdecentwork@gmail.com>
2023-01-20 10:48:04 +0200

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,2 @@
-// Selectors - це ф-ї які допомагають нам щось повертати з нашого стейта
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
